b82db73370020b90ee3f9060714dd38297b63cb6
Yaroslav Olenenko <jaroslavo@softwareplanet.uk.com>
2015-06-05 09:21:41 +0300

    fix some bug

--- src/main/java/com/yaroslav/other/calendar/FileManager.java
+++ src/main/java/com/yaroslav/other/calendar/FileManager.java
@@ -19,7 +19,7 @@ public class FileManager {
 
     public void saveToFile(String fileName, String content) {
         if (fileName.contains(File.separator)) {
-            String[] partPath = fileName.split(File.separator + File.separator);
+            String[] partPath = fileName.split(File.separator);
             createDirIfNotExist(partPath[0]);
             createDirIfNotExist(partPath[0] + File.separator + partPath[1]);
         }
